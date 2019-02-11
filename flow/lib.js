187382a3839f27cd8bc3522d1934970433fd70ba
guotiexin@gmail.com <guotiexin@gmail.com>
2019-02-11 17:40:10 +0100

    add mongo/swagger into flow

--- flow/lib.js
+++ flow/lib.js
@@ -1,3 +1,11 @@
 declare module 'fastify' {
 	declare export default any;
 }
+
+declare module 'mongodb' {
+	declare export default any;
+}
+
+declare module 'fastify-swagger' {
+	declare export default any;
+}
