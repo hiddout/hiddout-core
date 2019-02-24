5ca7854f46e95828630c8a9a9d8a21b9c4f08a0c
TyrealGray <tyrealgray@ymail.com>
2019-02-24 16:57:12 +0000

    add user existence check

--- src/routes/v1/signup.js
+++ src/routes/v1/signup.js
@@ -3,11 +3,41 @@
 import * as sjcl from 'sjcl';
 
 import { SUCCESS } from '../../static/serverMessage';
-import { dbCollectionInsertOne } from '../../db/client';
+import { dbCollectionFind, dbCollectionInsertOne } from '../../db/client';
+
+async function userNameCheckHandler(req: Object, reply: Object): Object {
+	try {
+		const userInfo = await dbCollectionFind('users', {
+			user: { $eq: req.query.user },
+		});
+
+		let isUsed = false;
+
+		if (userInfo.length) {
+			isUsed = true;
+		}
+
+		reply.type('application/json').code(200);
+		return { isUsed: isUsed, msg: SUCCESS };
+	} catch (err) {
+		console.log(err.stack);
+		reply.type('application/json').code(500);
+		return { msg: err };
+	}
+}
 
 async function signUpHandler(req: Object, reply: Object): Object {
 	try {
 		const timeNow = new Date().getTime();
+
+		const userInfo = await dbCollectionFind('users', {
+			user: { $eq: req.body.user },
+		});
+
+		if (userInfo.length) {
+			reply.type('application/json').code(200);
+			return { isUsed: true, token:null, msg: SUCCESS };
+		}
 
 		const saltBits = sjcl.random.randomWords(16);
 
@@ -15,8 +45,6 @@ async function signUpHandler(req: Object, reply: Object): Object {
 
 		const userKey = sjcl.codec.hex.fromBits(derivedKey);
 		const salt = sjcl.codec.hex.fromBits(saltBits);
-
-		// const re = sjcl.codec.hex.toBits(salt);
 
 		await dbCollectionInsertOne('users', {
 			user: req.body.user,
@@ -28,8 +56,7 @@ async function signUpHandler(req: Object, reply: Object): Object {
 		const token = await this.jwt.sign(req.body);
 
 		reply.type('application/json').code(200);
-		return { token:token, msg: SUCCESS };
-
+		return { isUsed: false, token: token, msg: SUCCESS };
 	} catch (err) {
 		console.log(err.stack);
 		reply.type('application/json').code(500);
@@ -38,6 +65,25 @@ async function signUpHandler(req: Object, reply: Object): Object {
 }
 
 function signup(fastify: fastify, opts: Object, next: () => any): void {
+	fastify.route({
+		method: 'GET',
+		url: '/signup',
+		schema: {
+			querystring: {
+				user: { type: 'string' },
+			},
+			response: {
+				'200': {
+					type: 'object',
+					properties: {
+						isUsed: { type: 'string' },
+					},
+				},
+			},
+		},
+		handler: userNameCheckHandler.bind(fastify),
+	});
+
 	fastify.route({
 		method: 'POST',
 		url: '/signup',
@@ -54,7 +100,8 @@ function signup(fastify: fastify, opts: Object, next: () => any): void {
 				'200': {
 					type: 'object',
 					properties: {
-						token: { type: 'string' },
+						isUsed: { type: 'boolean' },
+						token: {type: 'string'},
 					},
 				},
 			},
